d65802426c11145e139a64b6678bcbbb713e8455
Bharadwaj Duggaraju <bharadwaj.duggaraju@outlook.com>
2021-10-02 21:45:43 -0700

    about home padding

--- web/src/components/AboutHome.tsx
+++ web/src/components/AboutHome.tsx
@@ -11,17 +11,15 @@ const AboutHome: React.FC<Props> = ({ aboutBlob }) => {
   return (
     <section>
       <h1 className="text-3xl text-center pt-10">Our Mission</h1>
-      <div className="flex justify-start w-4/5 mx-auto lg:my-5 mt-5  flex-col lg:flex-row max-w-7xl" >
+      <div className="flex justify-start w-4/5 mx-auto lg:my-5 mt-5  flex-col lg:flex-row max-w-7xl">
         <img
           src={cmsImg(aboutBlob.image)}
           alt="Jagathi"
-          style={{maxHeight: "300px" }}
+          style={{ maxHeight: "300px" }}
           className=" lg:max-w-md object-center rounded shadow-sm"
         />
-        <div className="lg:px-8 text-lg text-left lg:w-auto ">
-          <p>
-            {aboutBlob.description}
-          </p>
+        <div className="lg:px-8 text-lg py-5 lg:py-0 text-left lg:w-auto ">
+          <p>{aboutBlob.description}</p>
           <Link to="/about">
             <button className="my-5 bg-yellow-500 text-white rounded px-5 lg:py-1 py-3">
               Read More...
