988160b1d73823dea3a3089c407cd3a6643aaf67
Bharadwaj Duggaraju <bharadwaj.duggaraju@outlook.com>
2021-10-09 22:34:36 -0700

    local changes

--- web/src/components/AboutHome.tsx
+++ web/src/components/AboutHome.tsx
@@ -11,14 +11,14 @@ const AboutHome: React.FC<Props> = ({ aboutBlob }) => {
   return (
     <section>
       <h1 className="text-3xl text-center pt-10">Our Mission</h1>
-      <div className="flex justify-start w-4/5 mx-auto lg:my-5 mt-5  flex-col lg:flex-row max-w-7xl">
+      <div className="flex justify-start w-4/5 lg:w-full mx-auto lg:my-5 mt-7   flex-col lg:flex-row max-w-7xl">
         <img
           src={cmsImg(aboutBlob.image)}
           alt="Jagathi"
-          style={{ maxHeight: "300px" }}
-          className=" lg:max-w-md object-center rounded shadow-sm"
+          style={{ maxHeight: "250px" }}
+          className=" lg:max-w-md object-top object-cover rounded shadow-sm lg:ml-10 "
         />
-        <div className="lg:px-8 text-lg py-5 lg:py-0 text-left lg:w-auto ">
+        <div className="lg:px-8 text-lg py-5 lg:py-0 md:text-left text-center lg:w-auto ">
           <p>{aboutBlob.description}</p>
           <Link to="/about">
             <button className="my-5 bg-yellow-500 text-white rounded px-5 lg:py-1 py-3">
--- web/src/components/AboutUs.tsx
+++ web/src/components/AboutUs.tsx
@@ -51,46 +51,56 @@ const AboutUs: React.FC<Props> = ({ aboutFullContent }) => {
         </div> */}
         <p className="text-left">
           <h1 className="mb-4 text-black text-2xl">Women Welfare</h1>
-          <p>Women must possess the self worth, confidence and freedom to choose
-          what they may with regard to their private and professional choices
-          alike.A proper nurturing, polishing and sharpening of those skills is
-          only made possible through better education, awareness and a space to
-          freely express oneself. Empowerment of women is a necessity for the
-          very development of a society.it helps women to promote an overall
-          development of society, grant women economic independence, reduce
-          scope domestic violence and sexual exploitation,eradicate corruption,
-          get rid of poverty and in-turn facilitate national development.</p>
+          <p>
+            Women must possess the self worth, confidence and freedom to choose
+            what they may with regard to their private and professional choices
+            alike.A proper nurturing, polishing and sharpening of those skills
+            is only made possible through better education, awareness and a
+            space to freely express oneself. Empowerment of women is a necessity
+            for the very development of a society.it helps women to promote an
+            overall development of society, grant women economic independence,
+            reduce scope domestic violence and sexual exploitation,eradicate
+            corruption, get rid of poverty and in-turn facilitate national
+            development.
+          </p>
           <h1 className="my-4 text-black text-2xl">Education</h1>
-          <p><blockquote className="my-4 font-bold text-xl">"Education is the most powerful weapon which you can use to
-          change the world" -Nelson Mandela</blockquote> The conventional assembly of
-          education inputs is not improving learning outcomes. This presents a
-          fundamental challenge to the way that governments, development
-          partners and communities are managing and supporting education
-          systems. A new, more radical approach that focusses on enhancing
-          learning outcomes is long overdue and forms the basis for this
-          strategy. Jagathi Foundation outlines the shift towards a greater
-          focus on improving learning outcomes, including supporting the breadth
-          of skills that allow young people to become agile, adaptive learners
-          and citizens, equipped to navigate personal, social, academic,
-          economic and environmental challenges.</p>
-          <h1 className="my-4 text-black text-2xl">Health</h1> 
-          <p>Number of people suffering from the major and severe health problems in rural sections
-          and urban slums. The biggest reason of these health disparities here
-          is malnutrition and pathetic slum living facilities. The air they
-          breathe, the water they drink and even food they eat are totally
-          contrary and pathetic to the normal and healthy living facilities.
-          Jagathi Foundation support basic health and social needs of
-          underprivileged sections of the society like poor, women, elderly,
-          children etc with awareness on hygiene, nutrition needs and lifestyle
-          changes.</p> 
-          <h1 className="my-4 text-black text-2xl">Environment</h1> 
-          <p>Jagathi Foundation sensitize the people about
-          value and importance of the environment Create awareness on how to
-          reduce the usage of plastic and pesticides which are harmful for
-          health. Proper conservation and utilization of Natural resources like
-          water & soil and protect the lakes, ponds from polluting. Also
-          rejuvenate wells & Ponds, bring them back to life for alternate source
-          of water and educate to do proper disposal of solid waste.
+          <p>
+            <blockquote className="my-4 font-bold text-xl">
+              "Education is the most powerful weapon which you can use to change
+              the world" -Nelson Mandela
+            </blockquote>{" "}
+            The conventional assembly of education inputs is not improving
+            learning outcomes. This presents a fundamental challenge to the way
+            that governments, development partners and communities are managing
+            and supporting education systems. A new, more radical approach that
+            focusses on enhancing learning outcomes is long overdue and forms
+            the basis for this strategy. Jagathi Foundation outlines the shift
+            towards a greater focus on improving learning outcomes, including
+            supporting the breadth of skills that allow young people to become
+            agile, adaptive learners and citizens, equipped to navigate
+            personal, social, academic, economic and environmental challenges.
+          </p>
+          <h1 className="my-4 text-black text-2xl">Health</h1>
+          <p>
+            Number of people suffering from the major and severe health problems
+            in rural sections and urban slums. The biggest reason of these
+            health disparities here is malnutrition and pathetic slum living
+            facilities. The air they breathe, the water they drink and even food
+            they eat are totally contrary and pathetic to the normal and healthy
+            living facilities. Jagathi Foundation support basic health and
+            social needs of underprivileged sections of the society like poor,
+            women, elderly, children etc with awareness on hygiene, nutrition
+            needs and lifestyle changes.
+          </p>
+          <h1 className="my-4 text-black text-2xl">Environment</h1>
+          <p>
+            Jagathi Foundation sensitize the people about value and importance
+            of the environment Create awareness on how to reduce the usage of
+            plastic and pesticides which are harmful for health. Proper
+            conservation and utilization of Natural resources like water & soil
+            and protect the lakes, ponds from polluting. Also rejuvenate wells &
+            Ponds, bring them back to life for alternate source of water and
+            educate to do proper disposal of solid waste.
           </p>
         </p>
       </div>
